27c467183c1efffbdfd887a8bf3d4fb7f4116535
Giavonator <gioalvez33@gmail.com>
2024-07-18 20:33:06 -0400

    Create Shell Script to run the Application

---
+++ runSudoku.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+cd /Users/Giani/Documents/"Programming Files"/Sudoku-Application/src/sudoku/
+/usr/bin/env /Library/Java/JavaVirtualMachines/temurin-17.jdk/Contents/Home/bin/java @/var/folders/58/3x193cr16fs4s5jwgl1knpmc0000gp/T/cp_ccbhr99r6qf4zp6whnyb5xogu.argfile -m sudoku/sudoku.Main
